4bca3267bad0f68a3c0b2e1f34424d9929195460
yonathanmorales@gmail.com <yonathanmorales@gmail.com@2d3cabc2-b7e0-77d2-1e9a-14326229f3ce>
2013-10-19 22:04:26 +0000

    Proyecto Final Compiladores e Interpretes

---
+++ trunk/ThinIce-Interpreter/src/thinice/interpreter/ThinIceInterpreter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thinice.interpreter;
+
+/**
+ *
+ * @author yonathanmorales
+ */
+public class ThinIceInterpreter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
