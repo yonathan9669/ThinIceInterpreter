7b7a84db7aa13ca4d1cd9bdfc9fa3552c2114044
yonathanmorales@gmail.com <yonathanmorales@gmail.com@2d3cabc2-b7e0-77d2-1e9a-14326229f3ce>
2013-10-22 15:47:20 +0000

    Configurador ThinIce-Test para pruebas usando como libreria el proyecto principal

--- ThinIce-Test/src/thinice/test/ThinIceTest.java
+++ ThinIce-Test/src/thinice/test/ThinIceTest.java
@@ -1,19 +1,39 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package thinice.test;
 
-/**
- *
- * @author Oscar
- */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java_cup.runtime.Symbol;
+import thinice.lexer.ThinIceLexer;
+import thinice.parser.ThinIceTokenDef;
+
 public class ThinIceTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FileInputStream input = null;
+
+        try {
+            input = new FileInputStream(args[0]);
+
+            FileReader f = new FileReader(args[0]);
+            char[] buf = new char[1024];
+            int n = f.read(buf);
+            
+            System.out.println(new String(buf, 0, n));
+            
+            ThinIceLexer lexer = new ThinIceLexer(input);
+
+            Symbol symbol;
+            while ((symbol = lexer.next_token()).sym != ThinIceTokenDef.EOF) {
+                System.out.println(symbol.value.toString());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se pudo abrir el archivo de entrada");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Usar: lexer simpleMoves.ti");
+        } catch(IOException ex){
+            System.out.println("Ha ocurrido un error de entrada y salida en el analizador léxico");
+        }
     }
 }
