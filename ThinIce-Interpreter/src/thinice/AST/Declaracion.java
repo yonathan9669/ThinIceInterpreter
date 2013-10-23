49ef14cd45f90f6e88a77531b942adfda68920dc
somezombie@gmail.com <somezombie@gmail.com@2d3cabc2-b7e0-77d2-1e9a-14326229f3ce>
2013-10-23 06:36:19 +0000

    Clases de nodos de arbol acomodadas

--- ThinIce-Interpreter/src/thinice/AST/Declaracion.java
+++ ThinIce-Interpreter/src/thinice/AST/Declaracion.java
@@ -2,29 +2,15 @@ package thinice.AST;
 
 import java.io.PrintStream;
 
-public class Declaracion extends Sentencia{
+public abstract class Declaracion extends Sentencia{
     //---------------------------Protected Attributes-----------------------------
     // <editor-fold desc="Private Attributes">
     protected TipoVariable tipo;
-    protected Variable id;
     //  </editor-fold>
     //---------------------------Constructors-----------------------------------
     // <editor-fold defaultstate="collapsed" desc="Constructors">
-    public Declaracion(TipoVariable tipo, Variable id, int linea, int columna) {
+    public Declaracion(int linea, int columna) {
         super(linea, columna);
-        this.tipo = tipo;
-        this.id = id;
     }
     //  </editor-fold>
-    //---------------------------Override Methods------------------------------- 
-    // <editor-fold defaultstate="collapsed" desc="Override Methods">
-    @Override
-    public void dump(PrintStream out, int n) {
-        dumpLineaColumna(out, n);
-        out.println("_asignacion");
-        tipo.dump(out, n + 2);
-        id.dump(out, n + 2);
-    }
-    //  </editor-fold>
-    
 }
--- ThinIce-Interpreter/src/thinice/AST/Vector.java
+++ ThinIce-Interpreter/src/thinice/AST/Vector.java
@@ -1,26 +1,33 @@
 package thinice.AST;
 
 import java.io.PrintStream;
+import thinice.TS.SimboloAbstracto;
+import thinice.util.Utilidades;
 
-public class Vector extends NodoLista<Variable>{
-    //---------------------------Protected Attributes-----------------------------
+public class Vector extends Expresion{
+    //---------------------------Protected Attributes---------------------------
     // <editor-fold desc="Private Attributes">
+    protected Variable var;
     protected Expresion pos;
     //  </editor-fold>
     
     //---------------------------Constructors-----------------------------------
     // <editor-fold defaultstate="collapsed" desc="Constructors">
-    public Vector() {
-        super();
+    public Vector(Variable var, Expresion expr, int linea, int columna) {
+        super(linea, columna);
+        this.var = var;
+        this.pos = expr;
     }
     //  </editor-fold>
     //---------------------------Override Methods------------------------------- 
     // <editor-fold defaultstate="collapsed" desc="Override Methods">
     @Override
     public void dump(PrintStream out, int n) {
-        for(Variable v: lista){
-            v.dump(out, n);
-        }
+        dumpLineaColumna(out, n);
+        out.println("_vector");
+        if(tipo_expr!=null) out.println(Utilidades.pad(n+2)+"tipo_expr: "+tipo_expr);
+        var.dump(out, n+2);
+        pos.dump(out, n+2);
     }
     //---------------------------------------
     //  </editor-fold>
