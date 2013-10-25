6b3ed23aa7d342dbd24832402985266a9cf8fe78
somezombie@gmail.com <somezombie@gmail.com@2d3cabc2-b7e0-77d2-1e9a-14326229f3ce>
2013-10-25 18:43:02 +0000

    Arreglo de literales enteros. Ahora pueden ser negativos. Se acomodó el CUP y el nodo Entero.

--- ThinIce-Interpreter/src/thinice/AST/Entero.java
+++ ThinIce-Interpreter/src/thinice/AST/Entero.java
@@ -5,10 +5,15 @@ import thinice.TS.SimboloAbstracto;
 import thinice.util.Utilidades;
 
 public class Entero extends Constante{
+    //---------------------------Private Attributes-----------------------------
+    // <editor-fold desc="Private Attributes">
+    protected String signo;
+    //  </editor-fold>
     //---------------------------Constructors-----------------------------------
     // <editor-fold defaultstate="collapsed" desc="Constructors">
-    public Entero(SimboloAbstracto token, int linea, int columna) {
+    public Entero(SimboloAbstracto token, int linea, int columna, String signo) {
         super(linea, columna, token);
+        this.signo = signo;
     }
     //  </editor-fold>
     //---------------------------Override Methods------------------------------- 
@@ -17,7 +22,8 @@ public class Entero extends Constante{
     @Override
     public void dump(PrintStream out, int n) {
         dumpLineaColumna(out, n);
-        out.println("_entero");
+        out.print("_entero_");
+        out.println(signo);
         if(tipo_expr!=null) out.println(Utilidades.pad(n+2)+"tipo_expr: "+tipo_expr);
         dump_SimboloAbstracto(out, n + 2, token);
     }
