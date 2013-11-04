597655fb639641be988f635a43bcfef12d2fb4d4
yonathanmorales@gmail.com <yonathanmorales@gmail.com@2d3cabc2-b7e0-77d2-1e9a-14326229f3ce>
2013-11-04 08:15:42 +0000

    Interfaz grafica completada, falta log para utilizar como salida de errores

--- ThinIce-Interpreter/src/Game/Parameters.java
+++ ThinIce-Interpreter/src/Game/Parameters.java
@@ -1,37 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Game;
 
-/**
- *
- * @author Moises
- */
 public interface Parameters {
-    
+
     public static String path_box = "Resources/BoxImage/";
     public static String path_table = "Resources/tableImage/";
     public static String path_level = "Resources/Worlds/";
     public static String path_world = "Resources/Worldimage/";
-    public static String [] box_images = {"hielo.png",
-                                          "usado.png",
-                                          "libre.png",
-                                           "nada.png",
-                                           "der.png",
-                                           "izq.png",
-                                           "arriba.png",
-                                           "abajo.png"};
-    
-    public static String [] world_images={"world1.png",
-                                          "world2.png",
-                                          "world3.png"};
-    public static String [] levels = {"level1.in",
-                                          "level2.in",
-                                          "level3.in"};
-    
-    
-    
+    //--------------------------------------
+    public static String[] box_images = {"nada.png", "libre.png", "usado.png", "hielo.png"};
+    public static String[] pac_images = {"arriba.png", "abajo.png", "izq.png", "der.png"};
+    //--------------------------------------
+    public static String[] world_images = {"world1.png", "world2.png", "world3.png"};
+    public static String[] levels = {"level1.in", "level2.in", "level3.in"};
 }
--- ThinIce-Interpreter/src/Game/Table.java
+++ ThinIce-Interpreter/src/Game/Table.java
@@ -6,16 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 public class Table extends JPanel {
 
-    public static final int HIELO = 0;
-    public static final int USADO = 1;
-    public static final int LIBRE = 2;
-    public static final int NADA = 3;
+    public static final int NADA = 0;
+    public static final int LIBRE = 1;
+    public static final int USADO = 2;
+    public static final int HIELO = 3;
     public static final int PINGUINO = 4;
-
     private Box[][] mapa;
     private Pinguin pinguino;
 
@@ -44,7 +45,8 @@ public class Table extends JPanel {
                             this.add(pinguino);
                             letter = LIBRE;
                         }
-                        this.mapa[letra][index] = new Box(letra, index, letter, this);
+                        this.mapa[letra][index] = new Box(letra, index, letter);
+                        this.add(this.mapa[letra][index]);
                     }
                 }
             }
@@ -54,116 +56,34 @@ public class Table extends JPanel {
         this.repaint();
     }
 
-    public void mirarArriba() {
-        pinguino.setDireccion(Pinguin.arriba);
+    public void mirar(int direccion) {
+        pinguino.setDireccion(direccion);
         this.pinguino.updateImage();
-        //pinguino.setMoverX(0);
-        //pinguino.setMoverY(-1);
     }
 
-    public void mirarAbajo() {
-        pinguino.setDireccion(Pinguin.abajo);
-        this.pinguino.updateImage();
-        //pinguino.setMoverX(0);
-        //pinguino.setMoverY(1);
-    }
-
-    public void mirarIzquierda() {
-        pinguino.setDireccion(Pinguin.izquierda);
-        this.pinguino.updateImage();
-        //pinguino.setMoverX(-1);
-        //pinguino.setMoverY(0);
-    }
-
-    public void mirarDerecha() {
-        pinguino.setDireccion(Pinguin.derecha);
-        this.pinguino.updateImage();
-        //pinguino.setMoverX(1);
-        //pinguino.setMoverY(0);
-    }
-
-    public void avanzar(int pasos) {
-
-        if (pasos > 0) {
-            int dx = this.pinguino.getMoverX();
-            int dy = this.pinguino.getMoverY();
-            for (int i = this.pinguino.getPosX(), j = this.pinguino.getPosY();
-                    pasos > 0 && (i >= 0 && i < 20) && (j >= 0 && j < 20);
-                    i += dx, j += dy, pasos--) {
-                if(this.mapa[i][j].getTipoCasilla() == LIBRE){
-                    this.pinguino.setPosX(i);
-                    this.pinguino.setPosY(j);
-                    this.mapa[i - dx][j - dy].setTipoCasilla(USADO);
-                    this.mapa[i - dx][j - dy].updateImage();
-                }
-                else{
+    public void avanzar(int steps) {
+        if (steps > 0) {
+            int dx = pinguino.getMoverX();
+            int dy = pinguino.getMoverY();
+            for (int i = pinguino.getPosX() + dx, j = pinguino.getPosY() + dy;
+                    steps > 0 && (i >= 0 && i < 20) && (j >= 0 && j < 20);
+                    i += dx, j += dy, steps--) {
+                if (mapa[i][j].getTipoCasilla() == LIBRE) {
+                    try {
+                        pinguino.setPosX(i);
+                        pinguino.setPosY(j);
+                        pinguino.updateLocation();
+                        mapa[i - dx][j - dy].setTipoCasilla(USADO);
+                        mapa[i - dx][j - dy].updateImage();
+                        repaint();
+                        Thread.sleep(250);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Table.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
                     break;
                 }
             }
-            this.pinguino.updateLocation();
-            this.repaint();
-            this.getParent().repaint();
-            /*
-             int posX = pinguino.getPosX();
-             int posY = pinguino.getPosY();
-             int moverX = pinguino.getMoverX();
-             int moverY = pinguino.getMoverY();
-
-            
-             int esperadaX = 0;
-             int esperadaY = 0;
-
-             int posibleX = posX;
-             int posibleY = posY;
-
-             if (moverX == 1) {
-             if (posX + pasos < mapa[0].length)
-             esperadaX = posX + pasos;
-             for (posibleX = posX; posibleX < mapa[0].length - 1; posibleX++) {
-             if (mapa[posibleX + 1][posibleY].getTipoCasilla() != LIBRE) {
-             break;
-             }
-             }
-             } else if (moverX == -1) {
-             if (posX - pasos >= 0)
-             esperadaX = posX - pasos;
-             for (posibleX = posX; posibleX > 0; posibleX--) {
-             if (mapa[posibleX - 1][posibleY].getTipoCasilla() != LIBRE) {
-             break;
-             }
-             }
-             } else if (moverY == 1) {
-             if (posY + pasos < mapa.length)
-             esperadaY = posY + pasos;
-             for (posibleY = posY; posibleY < mapa.length - 1; posibleY++) {
-             if (mapa[posibleX][posibleY + 1].getTipoCasilla() != LIBRE) {
-             break;
-             }
-             }
-             } else if (moverY == -1) {
-             if (posY - pasos >= 0)
-             esperadaY = posY - pasos;
-             for (posibleY = posY; posibleY > 0; posibleY--) {
-             if (mapa[posibleX][posibleY - 1].getTipoCasilla() != LIBRE) {
-             break;
-             }
-             }
-             }
-
-             if (esperadaX >= posibleX && esperadaX >= 0) {
-             pinguino.setPosX(posibleX);
-             } else if (posibleX > 0) {
-             pinguino.setPosX(esperadaX);
-             }
-
-             if (esperadaY >= posibleY && esperadaY >= 0) {
-             pinguino.setPosY(posibleY);
-             } else if (posibleY > 0) {
-             pinguino.setPosY(esperadaY);
-             }
-             */
-            this.repaint();
         }
     }
-
 }
